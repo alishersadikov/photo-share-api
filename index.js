a23cc9035addff714dc36604c3be129d102627e8
Alisher Sadikov <alishersadikov@gmail.com>
2019-11-05 07:47:47 -0600

    Enable graphql playground

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require('apollo-server-express')
 const express = require('express')
+const expressPlayground = require('graphql-playground-middleware-express').default
 
 const typeDefs = `
   scalar DateTime
@@ -145,6 +146,7 @@ var app = express()
 const server = new ApolloServer({ typeDefs, resolvers })
 
 app.get('/', (req, res) => res.end('Welcome to the PhotoShare API'))
+app.get('/playground', expressPlayground({ endpoint: '/graphql' }))
 
 app.listen({ port: 4000 }, () =>
   console.log(`GraphQL Server running @ http://localhost:4000${server.graphqlPath}`)
